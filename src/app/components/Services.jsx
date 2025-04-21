e1888a6c8b6b08dd31101e054b1353c2b7646f98
Isaac Orija <isaac.orija@gmail.com>
2025-04-21 19:34:19 +0100

    new nav changes

--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -2,37 +2,37 @@
 
 import React from "react";
 import { MoveUpRight } from "lucide-react";
-
+import "./Services.css";
 const Services = () => {
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
-        <div className=" rounded-xl p-6 shadow">
+        <div className=" rounded-xl p-6">
           {" "}
           <div className="flex justify-center items-center gap-6 p-4">
-            <div className="service-number px-6 py-4 rounded-xl shadow">
+            <div className="service-number px-6 py-4">
               <span>01</span>
             </div>
-            <div className="service-title px-6 py-4 rounded-xl shadow">
+            <div className="service-title px-6 py-4">
               <span>Content Production</span>
             </div>
-            <div className="service-icon px-6 py-4 rounded-xl shadow">
+            <div className="service-icon px-6 py-4">
               <span>
                 <MoveUpRight />
               </span>
             </div>
           </div>
         </div>
-        <div className="rounded-xl p-6 shadow">
+        <div className="rounded-xl p-6">
           {" "}
           <div className="flex justify-center items-center gap-6 p-4">
-            <div className="service-number px-6 py-4 shadow">
+            <div className="service-number px-6 py-4">
               <span>02</span>
             </div>
-            <div className="service-title px-6 py-4 shadow">
+            <div className="service-title px-6 py-4">
               <span>Creative Direction</span>
             </div>
-            <div className="service-icon px-6 py-4 shadow">
+            <div className="service-icon px-6 py-4">
               <span>
                 <MoveUpRight />
               </span>
@@ -42,29 +42,29 @@ const Services = () => {
         <div className=" rounded-xl p-6 shadow">
           {" "}
           <div className="flex justify-center items-center gap-6 p-4">
-            <div className="service-number px-6 py-4 rounded-xl shadow">
+            <div className="service-number px-6 py-4">
               <span>03</span>
             </div>
-            <div className="service-title px-6 py-4 rounded-xl shadow">
+            <div className="service-title px-6 py-4">
               <span>Film Production</span>
             </div>
-            <div className="service-icon px-6 py-4 rounded-xl shadow">
+            <div className="service-icon px-6 py-4">
               <span>
                 <MoveUpRight />
               </span>
             </div>
           </div>
         </div>
-        <div className=" rounded-xl p-6 shadow">
+        <div className=" rounded-xl p-6">
           {" "}
           <div className="flex justify-center items-center gap-6 p-4">
-            <div className="service-number px-6 py-4  shadow">
+            <div className="service-number px-6 py-4">
               <span>04</span>
             </div>
-            <div className="service-title px-6 py-4 shadow">
+            <div className="service-title px-6 py-4">
               <span>Music & Sound Production</span>
             </div>
-            <div className="service-icon px-6 py-4  shadow">
+            <div className="service-icon px-6 py-4">
               <span>
                 <MoveUpRight />
               </span>
